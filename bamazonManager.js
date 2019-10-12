2d0dfbfa4b20339cddb3564f6c4f5a58f7778e27
dyl trimble <dyltrimble12@gmail.com>
2019-10-11 23:37:57 -0400

    manager function created

--- bamazonManager.js
+++ bamazonManager.js
@@ -17,7 +17,45 @@ var connection = mysql.createConnection({
 // promptManagerAction will present menu options to the manager and trigger appropriate logic
 function promptManagerAction() {
 
-	// Prompt the manager to select an option
-
-    // .then conditional statements to run through manager functions
+    // Prompt the manager to select an option
+    inquirer.prompt([
+		{
+			type: 'list',
+			name: 'option',
+			message: 'Please select an option:',
+			choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product'],
+			filter: function (val) {
+				if (val === 'View Products for Sale') {
+					return 'sale';
+				} else if (val === 'View Low Inventory') {
+					return 'lowInventory';
+				} else if (val === 'Add to Inventory') {
+					return 'addInventory';
+				} else if (val === 'Add New Product') {
+					return 'newProduct';
+				} else {
+					console.log('ERROR: Unsupported operation!');
+					exit(1);
+				}
+			}
+        }
+        // .then conditional statements to run through manager functions
+	]).then(function(input) {
+        // console.log('User has selected: ' + JSON.stringify(input));
+        
+		if (input.option ==='sale') {
+			displayInventory();
+		} else if (input.option === 'lowInventory') {
+			displayLowInventory();
+		} else if (input.option === 'addInventory') {
+			addInventory();
+		} else if (input.option === 'newProduct') {
+			createNewProduct();
+		} else {
+			// This case should be unreachable
+			console.log('ERROR: Unsupported operation!');
+			exit(1);
+        }
+        //switch statement in place of this.
+	});
 };
